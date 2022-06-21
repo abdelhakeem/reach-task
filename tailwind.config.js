6919a8e296ff39bb14da2bf3f8a1fb06eab2e8d6
Abdelhakeem <abdelhakeem.osama@hotmail.com>
2022-06-22 00:16:39 +0200

    Add daisyUI and color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,29 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: "#000e37",
+          secondary: "#0076be",
+          accent: "#1192d4",
+          neutral: "#575556",
+          "base-100": "#ffffff",
+          "base-content": "#000000",
+        },
+      },
+      {
+        dark: {
+          primary: "#000e37",
+          secondary: "#0076be",
+          accent: "#1192d4",
+          neutral: "#575556",
+          "base-100": "#000000",
+          "base-content": "#ffffff",
+        },
+      },
+    ],
+  },
+};
